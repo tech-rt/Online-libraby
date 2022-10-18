252d160aed97044f908cf6d99ba498ab964f6772
Rishavagarwal12345 <91730977+Rishavagarwal12345@users.noreply.github.com>
2022-10-18 23:04:39 +0530

    solved error while login

--- src/com/javatpoint/verifyLogin2.java
+++ src/com/javatpoint/verifyLogin2.java
@@ -13,10 +13,31 @@ public static boolean checkLogin(String username,String password){
 		PreparedStatement ps=con.prepareStatement("Select * from LIBRARYADMIN where username = ? and password = ?");
 		//ps.setInt(1,accountno);
 		ps.setString(1,username);
-		ps.setString(2,password);
+		ps.setS\
+			Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the size of array");
+		int a = sc.nextInt();
+		int[] arr = new int[a];
+
+		System.out.println("Enter the array");
+
+		for (int i = 0; i < a; i++) {
+			arr[i] = sc.nextInt();
+		} 
 		
-		ResultSet rs=ps.executeQuery();
-		status=rs.next();
+		int sum = 0;
+		int max = arr[0];
+		
+		for (int i = 0; i < arr.length; i++) {
+			
+			sum = sum + arr[i];			
+			if(sum < 0)
+				sum = 0;
+			if(sum> max)
+				max = sum;
+			}
+		
 		
 	} catch (SQLException e) {
 		e.printStackTrace();
